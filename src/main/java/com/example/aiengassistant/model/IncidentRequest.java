0af46aca3acc9e33985ec4599535a2646d34ab43
Sangeetha <sangeethase2@gmail.com>
2026-07-02 21:01:52 -0400

    chore: create base project structure

---
+++ src/main/java/com/example/aiengassistant/model/IncidentRequest.java
@@ -0,0 +1,9 @@
+package com.example.aiengassistant.model;
+
+import lombok.Data;
+
+@Data
+public class IncidentRequest {
+
+    private String question;
+}
---
+++ src/main/java/com/example/aiengassistant/model/IncidentResponse.java
@@ -0,0 +1,9 @@
+package com.example.aiengassistant.model;
+
+import lombok.Data;
+
+@Data
+public class IncidentResponse {
+
+    private String message;
+}
